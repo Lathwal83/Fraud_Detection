541ed1cb1efa1cf7c99d6201162b5b7f1a9502be
Lathwal83 <lathwaladitya94@gmail.com>
2022-02-04 22:52:22 +0530

    model 2 is final model

--- model.py
+++ model.py
@@ -8,4 +8,6 @@ def model2():
     print(" adding code for ml model 2")
 
 def model3():
-    print(" adding code for ml model 3")    
+    print(" adding code for ml model 3")
+
+print(" model 2 is best")        
